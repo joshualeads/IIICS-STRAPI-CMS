93e92983efe750dcfae20b7e96dbbd73ab041ee9
joshualeads <joshuajabakumar@gmail.com>
2024-11-20 18:52:38 +0530

    Strapi Upgrade to 5.4.0

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -4,17 +4,13 @@
  */
 import strapiCloud from "@strapi/plugin-cloud/strapi-admin";
 import graphql from "@strapi/plugin-graphql/strapi-admin";
-import i18N from "@strapi/plugin-i18n/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
-import importExportEntries from "strapi-plugin-import-export-entries/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
 
 renderAdmin(document.getElementById("strapi"), {
   plugins: {
     "strapi-cloud": strapiCloud,
     graphql: graphql,
-    i18n: i18N,
     "users-permissions": usersPermissions,
-    "import-export-entries": importExportEntries,
   },
 });
